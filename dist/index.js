669379e160577b92e9a27b57d1ca8a21d036b1e4
Christopher Brum <christopher.brum@gmail.com>
2023-11-06 10:12:32 -0800

    compiled the index.js file

--- dist/index.js
+++ dist/index.js
@@ -1,43 +1,63 @@
-import { io } from 'socket.io-client';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { io } from "socket.io-client";
 export class Twine {
     constructor(host) {
-        this.socket = io(host);
-        this.sessionId = localStorage.getItem('twineSessionId') || '';
-        this.socket.on('connect', () => {
-            this.socket.emit('sessionId', this.sessionId);
-        });
-        this.socket.on('setSessionId', (sessionId) => {
-            console.log('client session event: ' + sessionId);
-            localStorage.setItem('twineSessionId', sessionId);
+        this.initializeTwine(host);
+    }
+    initializeTwine(host) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield fetch(`${host}/set-cookie`, { credentials: 'include' });
+            this.socket = io(host, {
+                withCredentials: true,
+                transports: ['websocket'],
+            });
+            this.socket.on('connect', () => __awaiter(this, void 0, void 0, function* () {
+                var _a;
+                console.log('Connected to the Twine server');
+                yield ((_a = this.socket) === null || _a === void 0 ? void 0 : _a.emit('stateRecovery'));
+            }));
         });
     }
     connect() {
-        this.socket.connect();
+        var _a;
+        (_a = this.socket) === null || _a === void 0 ? void 0 : _a.connect();
     }
     disconnect() {
-        this.socket.disconnect();
+        var _a;
+        (_a = this.socket) === null || _a === void 0 ? void 0 : _a.disconnect();
     }
     subscribe(roomsToJoin) {
-        this.socket.emit('join', roomsToJoin);
-        // should change the listener on the server to `subscribe`
-        // should accept an array of strings instead of a single string
+        var _a, _b;
+        (_a = this.socket) === null || _a === void 0 ? void 0 : _a.emit('subscribe', roomsToJoin);
+        (_b = this.socket) === null || _b === void 0 ? void 0 : _b.on("roomJoined", (msg) => console.log(msg));
     }
     unsubscribe(roomsToLeave) {
-        this.socket.emit('leave', roomsToLeave);
-        // need to create a listener on the server to ctach this event and unsubscribe
-        // param should be an array of strings
+        var _a, _b;
+        (_a = this.socket) === null || _a === void 0 ? void 0 : _a.emit('unsubscribe', roomsToLeave);
+        (_b = this.socket) === null || _b === void 0 ? void 0 : _b.on("roomLeft", (msg) => console.log(msg));
     }
-    listenFor(eventName, callback) {
-        this.socket.on(eventName, (payload) => {
-            callback(payload);
+    listenOn(roomName, callback) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const id = setInterval(() => {
+                if (this.socket) {
+                    this.socket.on("message", (payload) => {
+                        var _a;
+                        if (payload.room === roomName) {
+                            callback(payload);
+                            (_a = this.socket) === null || _a === void 0 ? void 0 : _a.emit("updateSessionTS", payload.timestamp);
+                        }
+                    });
+                    clearInterval(id);
+                }
+            }, 100);
         });
     }
 }
-const twineConnection = new Twine('http://localhost:3001');
-twineConnection.disconnect();
-twineConnection.connect();
-twineConnection.subscribe(['A', 'B']);
-twineConnection.unsubscribe(['B']);
-twineConnection.listenFor('message', (payload) => {
-    console.log(payload);
-});
